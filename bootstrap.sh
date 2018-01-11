b6fd45c21921c3497a7d3034f604166eb6a6728b
David Duncan <dduncan@intelliair.com>
2018-01-10 20:47:29 -0600

    Add bootstrap

---
+++ bootstrap.sh
@@ -0,0 +1,124 @@
+#!/usr/bin/env bash
+
+baselibs(){
+    sudo apt-get update -y
+    sudo apt-get upgrade -y
+    git config --global core.excludesfile ~/.gitignore.global
+}
+
+vim() {
+    sudo apt-get install vim
+}
+
+i3() {
+    sudo apt-get install i3 i3lock
+}
+
+chromium() {
+    sudo apt-get install chromium-browser
+}
+
+docker(){
+    sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+    sudo apt-key fingerprint 0EBFCD88
+
+    sudo add-apt-repository \
+       "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+       $(lsb_release -cs) \
+       stable"
+
+    sudo apt-get update
+    sudo apt-get install docker-ce
+    sudo groupadd docker
+    sudo usermod -aG docker $USER
+    curl -L https://github.com/docker/compose/releases/download/1.14.0/docker-compose-`uname -s`-`uname -m` > docker-compose && chmod +x docker-compose && sudo mv docker-compose /usr/local/bin/docker-compose
+
+}
+
+vundle(){
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    vim +PluginInstall +qall
+}
+
+prezto(){
+    clear
+    sudo apt-get install -y git
+    sudo apt-get update && sudo apt-get install -y zsh
+    # Get prezto
+    git clone --recursive https://github.com/sorin-ionescu/prezto.git ~/.zprezto
+
+    # Backup zsh config if it exists
+    if [ -f ~/.zshrc ];
+       then
+           mv ~/.zshrc ~/.zshrc.backup
+    fi
+
+    # Create links to zsh config files
+    ln -s ~/.zprezto/runcoms/zlogin ~/.zlogin
+    ln -s ~/.zprezto/runcoms/zlogout ~/.zlogout
+    ln -s ~/.zprezto/runcoms/zpreztorc ~/.zpreztorc
+    ln -s ~/.zprezto/runcoms/zprofile ~/.zprofile
+    ln -s ~/.zprezto/runcoms/zshenv ~/.zshenv
+    ln -s ~/.zprezto/runcoms/zshrc ~/.zshrc
+    chsh -s $(which zsh)
+}
+
+antigen(){
+    git clone https://github.com/zsh-users/antigen.git ~/antigen
+}
+
+ohmyzsh(){
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+}
+
+powerlinefont() {
+    git clone https://github.com/powerline/fonts.git
+    # install
+    cd fonts
+    ./install.sh
+    # clean-up a bit
+    cd ..
+    rm -rf fonts
+}
+
+powerline() {
+    sudo apt-get install python3.6
+    wget https://bootstrap.pypa.io/get-pip.py
+    sudo python get-pip.py
+    sudo pip install powerline-status
+}
+
+addkube() {
+	curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.7.0/bin/linux/amd64/kubectl
+	chmod +x ./kubectlsudo 
+	mv ./kubectl /usr/local/bin/kubectl
+}
+
+configureWatches() {
+    echo fs.inotify.max_user_watches=512000 | sudo tee -a /etc/sysctl.conf
+    sudo sysctl -p
+}
+
+baselibs
+vim
+i3
+chromium
+prezto
+powerlinefont
+powerline
+vundle
+docker  
+addkube
+configureWatches
+
+
+echo "==========================="
+echo "Everything went ok - Reboot!"
+0
