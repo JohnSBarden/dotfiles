6c43b14c168670ad94483e307d05434d4a1211a9
David Duncan <dduncan@intellifarms.com>
2018-12-15 09:36:50 -0600

    better maim ss, fixing kube bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -107,7 +107,7 @@ _powerline() {
 }
 
 _addkube() {
-  	curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.7.0/bin/linux/amd64/kubectl
+  	curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.8.3/bin/linux/amd64/kubectl
 	chmod +x ./kubectl
     sudo mv ./kubectl /usr/local/bin/kubectl
 }
