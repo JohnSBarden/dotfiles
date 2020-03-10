02d4af685a75d7fa7bdca401f836d8949d3eb67e
David Duncan <dduncan@intellifarms.com>
2020-03-10 16:07:04 -0500

    update kubectl version

--- bootstrap.sh
+++ bootstrap.sh
@@ -108,7 +108,7 @@ _powerline() {
 }
 
 _addkube() {
-  	curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.8.3/bin/linux/amd64/kubectl
+  	curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.17.0/bin/linux/amd64/kubectl
 	chmod +x ./kubectl
     sudo mv ./kubectl /usr/local/bin/kubectl
 }
