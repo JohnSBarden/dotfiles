8537e4a503644f767e919b44b9363c261baf02dc
David Duncan <dduncan@intellifarms.com>
2020-03-24 08:21:01 -0500

    fix aws-auth in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -116,7 +116,7 @@ _addKube() {
 
     # add aws-iam-authenticator too
 
-    curl -LO aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.15.10/2020-02-22/bin/linux/amd64/aws-iam-authenticator
+    curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.15.10/2020-02-22/bin/linux/amd64/aws-iam-authenticator
     chmod +x ./aws-iam-authenticator
     sudo mv ./aws-iam-authenticator /usr/local/bin/aws-iam-authenticator
 }
