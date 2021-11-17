e675f512b30f8c9152cfe062b539171113f05655
John Barden <john.barden@agisuretrack.com>
2021-11-17 15:18:39 -0600

    popup calendar

---
+++ scripts/popup-calendar.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+BAR_HEIGHT=20  # polybar height
+BORDER_SIZE=10  # border size from your wm settings
+YAD_WIDTH=222  # 222 is minimum possible value
+YAD_HEIGHT=193 # 193 is minimum possible value
+DATE="$(date +"%I:%M:%S %a, %b %d %Y")"
+
+case "$1" in
+--popup)
+    if [ "$(xdotool getwindowfocus getwindowname)" = "yad-calendar" ]; then
+        exit 0
+    fi
+
+    # Set X and Y by mouse location
+    eval "$(xdotool getmouselocation --shell)"
+    # Set WIDTH and HEIGHT to resolution
+    eval "$(xdotool getdisplaygeometry --shell)"
+
+    # X
+    if [ "$((X + YAD_WIDTH / 2 + BORDER_SIZE))" -gt "$WIDTH" ]; then #Right side
+        : $((pos_x = WIDTH - YAD_WIDTH - BORDER_SIZE))
+    elif [ "$((X - YAD_WIDTH / 2 - BORDER_SIZE))" -lt 0 ]; then #Left side
+        : $((pos_x = BORDER_SIZE))
+    else #Center
+        : $((pos_x = X - YAD_WIDTH / 2))
+    fi
+
+    # Y
+    if [ "$Y" -gt "$((HEIGHT / 2))" ]; then #Bottom
+        : $((pos_y = (HEIGHT) + 2*(YAD_HEIGHT + BAR_HEIGHT) + BORDER_SIZE))
+    else #Top
+        : $((pos_y = BAR_HEIGHT + BORDER_SIZE))
+    fi
+
+    yad --calendar --undecorated --fixed --close-on-unfocus --no-buttons \
+        --width="$YAD_WIDTH" --height="$YAD_HEIGHT" --posx="$pos_x" --posy="$pos_y" \
+        --title="yad-calendar" --borders=0 >/dev/null &
+    ;;
+*)
+    echo "$DATE"
+    ;;
+esac
+
